d32867ad759b410a7aac462dfb639241a4f01071
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-04-09 15:07:26 +0900

    Time: 3 ms (30.69%), Space: 18.2 MB (6%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,23 +1,20 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
-        Parentheses = ["(", ")"]
 
-        def dfs(cur_gen_parentheses, left_count, right_count):
-            if left_count < right_count or left_count > n or right_count > n:
-                return 
-            
+        def dfs(cur_gen_parentheses, left_count, right_count):            
             if left_count == n and right_count == n:
                 result.append("".join(cur_gen_parentheses))
                 return
 
-            for parenthese in Parentheses:
+            for parenthese in ["(", ")"]:
                 next_left_count =  left_count + 1 if parenthese == "(" else left_count
                 next_right_count =  right_count + 1 if parenthese == ")" else right_count
                 
-                cur_gen_parentheses.append(parenthese)
-                dfs(cur_gen_parentheses, next_left_count, next_right_count)
-                cur_gen_parentheses.pop()
+                if next_left_count >= next_right_count and next_left_count <= n and next_right_count <= n:
+                    cur_gen_parentheses.append(parenthese)
+                    dfs(cur_gen_parentheses, next_left_count, next_right_count)
+                    cur_gen_parentheses.pop()
                 
         dfs([], 0, 0)
 
