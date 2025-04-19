a3f797b05a5c26d54fcb036b122aacdefa84c905
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-04-19 22:02:56 +0900

    Time: 57 ms (40.81%), Space: 53.4 MB (62.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,10 +3,9 @@
  * @return {number}
  */
 var fib = function(n) {
-    let fib = [0, 1]
-    let start = 0
-    for (let i = 2; i <= 30; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2]
-    }
-    return fib[n]
+  if (n <= 1) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
 };
