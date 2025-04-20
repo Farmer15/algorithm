8ac392fc9444e602bc173af7224203d08ed1d8d7
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-04-20 14:33:31 +0900

    Time: 35 ms (25.31%), Space: 55.4 MB (35.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  const hashMap = new Map();
-
   for (let i = 0; i < nums.length; i++) {
-    if (hashMap.has(nums[i])) {
-      return [hashMap.get(nums[i]), i]
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
-
-    const needTarget = target - nums[i];
-
-    hashMap.set(needTarget, i);
   }
 };
