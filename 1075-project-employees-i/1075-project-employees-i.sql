b99b91c98ede02b680d88a239ea678e6a27bac38
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-04-30 20:21:04 +0900

    Time: 554 ms (46.26%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,5 +1,5 @@
 -- Write your PostgreSQL query statement below
-SELECT P.project_id, ROUND(avg(E.experience_years)::numeric, 3) AS average_years FROM Project P
+SELECT P.project_id, ROUND(avg(E.experience_years)::numeric, 2) AS average_years FROM Project P
 RIGHT OUTER JOIN Employee E
 ON P.employee_id = E.employee_id
 WHERE NOT P.project_id is NULL
