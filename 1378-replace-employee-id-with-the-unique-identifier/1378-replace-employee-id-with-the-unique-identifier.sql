8f53ef52ebfa603b5ebc62f2d5c03834acef8537
Yang Su Bo <156686089+Farmer15@users.noreply.github.com>
2025-07-30 19:01:11 +0900

    Time: 415 ms (99.92%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,8 @@
-SELECT unique_id, name
-FROM Employees
-    LEFT JOIN  EmployeeUNI
-    ON Employees.id = EmployeeUNI.id
+-- Write your PostgreSQL query statement below
 
+
+SELECT EmployeeUNI.unique_id, Employees.name
+FROM EmployeeUNI
+  FULL OUTER JOIN Employees
+  ON EmployeeUNI.id = Employees.id
+  WHERE NOT Employees.name is NULL
